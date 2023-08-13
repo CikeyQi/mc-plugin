bf8abe170888315ed65f1a573db6bcefa250afeb
0卡苏打水 <61369914+CikeyQi@users.noreply.github.com>
2023-08-13 16:36:34 +0800

    更新 WebSocket.js

--- components/WebSocket.js
+++ components/WebSocket.js
@@ -9,7 +9,7 @@ class WebSocket {
   }
 
   async WebSocket() {
-    const config = await Config.getConfig();
+    let config = await Config.getConfig();
     try {
       Init.initConfig();
       const wsServer = new WebSocketServer({
@@ -31,17 +31,17 @@ class WebSocket {
 
         wsServer.on('connection', (ws) => {
           Log.i('[MC_QQ]丨Minecraft Server 已连接至 WebSocket 服务器');
-          this.sendMsg('[MC_QQ]丨Minecraft Server 已连接至 WebSocket 服务器', config);
-          this.wsHandler(ws, config);
+          this.sendMsg('[MC_QQ]丨Minecraft Server 已连接至 WebSocket 服务器');
+          this.wsHandler(ws);
         });
       } catch (error) {
-        this.sendMsg('[MC_QQ]丨WebSocket服务启动失败，请检查控制台输出', config);
+        this.sendMsg('[MC_QQ]丨WebSocket服务启动失败，请检查控制台输出');
         Log.e(error);
         return false;
       }
     }
 
-  wsHandler(ws, config) {
+  wsHandler(ws) {
       ws.on('message', (message) => {
         let event;
         if (message instanceof Buffer) {
@@ -52,31 +52,32 @@ class WebSocket {
         }
         switch (event.event_name) {
           case 'PlayerQuitEvent':
-            this.sendMsg(`${event.player.nickname} 已退出游戏`, config);
+            this.sendMsg(`${event.player.nickname} 已退出游戏`);
             break;
           case 'PlayerJoinEvent':
-            this.sendMsg(`${event.player.nickname} 已加入游戏`, config);
+            this.sendMsg(`${event.player.nickname} 已加入游戏`);
             break;
           case 'PlayerDeathEvent':
-            this.sendMsg(`${event.player.nickname} ${event.death_message}`, config);
+            this.sendMsg(`${event.player.nickname} ${event.death_message}`);
             break;
           case 'AsyncPlayerChatEvent':
-            this.sendMsg(`${event.player.nickname} 说 ${event.message}`, config);
+            this.sendMsg(`${event.player.nickname} 说 ${event.message}`);
             break;
         }
       });
 
       ws.on('close', () => {
-        this.sendMsg('[MC_QQ]丨Minecraft Server 已断开 WebSocket 服务器', config);
+        this.sendMsg('[MC_QQ]丨Minecraft Server 已断开 WebSocket 服务器');
       });
 
       ws.on('error', (error) => {
-        this.sendMsg('[MC_QQ]丨Minecraft Server WebSocket 通信出现异常：' + error, config);
+        this.sendMsg('[MC_QQ]丨Minecraft Server WebSocket 通信出现异常：' + error);
         Log.e(error);
       });
     }
 
-    sendMsg(msg, config) {
+    sendMsg(msg) {
+      let config = await Config.getConfig();
       for (let i = 0; i < config.group_list.length; i++) {
         Bot.pickGroup(config.group_list[i]).sendMsg(msg);
       }

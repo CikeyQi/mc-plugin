05fe45b3d81d17fbf3387694f6952af14e673202
ZeroCal Su <3146312184@qq.com>
2024-04-08 14:16:32 +0800

    Update Setting.js

--- apps/Setting.js
+++ apps/Setting.js
@@ -1,175 +1,97 @@
 import plugin from '../../../lib/plugins/plugin.js'
-import Log from '../utils/logs.js'
 import Config from '../components/Config.js'
 import Init from '../model/init.js'
 
 export class Setting extends plugin {
   constructor() {
     super({
-      name: 'QQ_MC',
-      dsc: '配置参数',
+      /** 功能名称 */
+      name: 'MC_QQ-设置',
+      /** 功能描述 */
+      dsc: '设置插件',
       event: 'message',
+      /** 优先级，数字越小等级越高 */
       priority: 5000,
-      rule: [{
-        reg: '#?mc设置.*$',
-        fnc: 'setting',
-        permission: 'master'
-      }]
+      rule: [
+        {
+          /** 命令正则匹配 */
+          reg: '#?mc(开启|关闭)同步.*$',
+          /** 执行方法 */
+          fnc: 'setting',
+          /** 主人权限 */
+          permission: 'master'
+        }
+      ]
     })
-
   }
 
   async setting(e) {
+
+    if (!e.group_id) {
+      await e.reply('请在群内使用此功能')
+      return true
+    }
+
     Init.initConfig()
-    // 读取配置项
     var config = Config.getConfig()
-    const keyParam = /((ws|rcon)(路由|端口|状态|地址|密码)|群名显示|同步|命令响应头|乱码修复|屏蔽词)/g.exec(e.msg)
-    const key = keyParam ? keyParam[1] : ''
-    let value = e.msg.replace(/#?mc设置/, '').replace(new RegExp(`${key}`), '').trim()
-    // key匹配失败,value存在时
-    if (key === '' && value != '') {
-      e.reply(`配置项不存在,请检查输入`)
+
+    if (!config.mc_qq_server_list.length) {
+      await e.reply('请自行在配置文件中添加服务器相关信息再使用此功能')
       return true
     }
-    // 修改标志位,修改成功后修改为true
-    let alterFlag = false
-    switch (key) {
-      case 'ws路由':
-        if (value.startsWith('/')) {
-          config.mc_qq_ws_url = value
-          alterFlag = true
-        } else {
-          e.reply('请输入正确的路由地址,格式为\n#mc设置ws路由/yz/v3/mcqq', true)
-          return true
-        }
-        break
-      case 'ws端口':
-        config.mc_qq_ws_port = Number(value)
-        alterFlag = true
-        break
-      case 'rcon状态':
-        if (value.match(/(开启|关闭)/)) {
-          if (value === '开启') {
-            config.rcon_enable = true
-          } else if (value === '关闭') {
-            config.rcon_enable = false
-          }
-          alterFlag = true
-        }
-        break
-      case 'rcon地址':
-        if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(value)) {
-          e.reply('请输入正确的ip地址,格式为\n#mc设置rcon地址127.0.0.1', true)
-          return true
-        } else {
-          config.rcon_host = value
-          alterFlag = true
-        }
-        break
-      case 'rcon端口':
-        config.rcon_port = Number(value)
-        alterFlag = true
-        break
-      case 'rcon密码':
-        config.rcon_password = value
-        alterFlag = true
-        break
-      case '群名显示':
-        if (value.match(/(开启|关闭)/)) {
-          if (value === '开启') {
-            config.mc_qq_send_group_name = true
-          } else if (value === '关闭') {
-            config.mc_qq_send_group_name = false
-          }
-          alterFlag = true
-        }
-        break
-      case '同步':
-        // 如果值存在,找到该值的索引
-        const index = config['group_list'].indexOf(e.group_id)
-        if (value.match(/(开启|关闭)/)) {
-          if (value === '开启') {
-            if (index > -1) {
-              e.reply(`${e.group.name}(${e.group_id})群与服务器消息同步已开启`, true)
-              return true
-            } else {
-              config['group_list'].push(e.group_id)
-              alterFlag = true
-            }
-          } else if (value === '关闭') {
-            if (index > -1) {
-              config['group_list'].splice(index, 1)
-              alterFlag = true
-            } else {
-              e.reply(`${e.group.name}(${e.group_id})群与服务器消息同步已关闭`, true)
-              return true
-            }
-          }
-        }
-        break
-      case '命令响应头':
-        if (value) {
-          config.command_header = value
-          alterFlag = true
-        }
-        break
-      case '乱码修复':
-        if (value.match(/(开启|关闭)/)) {
-          if (value === '开启') {
-            config.is_garbled = true
-          } else if (value === '关闭') {
-            config.is_garbled = false
-          }
-          alterFlag = true
-          }
-        break
-      case '屏蔽词':
-        if (value.match('^/.*(/g)$')) {
-          config.mask_word = value
-          alterFlag = true
-        } else {
-          e.reply('请输入正确的正则表达式,格式为\n#mc设置屏蔽词/§./g', true)
-          return true
-        }
-        break
-      default:
-        // 如果key为空且value为空则展示
-        let str = [
-          '===Minecraft插件配置===' + '\n' +
-          'ws地址：' + config.mc_qq_ws_url + '\n' +
-          'ws端口：' + config.mc_qq_ws_port + '\n' +
-          'rcon状态：' + (config.rcon_enable ? '开启' : '关闭') + '\n' +
-          'rcon地址：' + config.rcon_host + '\n' +
-          'rcon端口：' + config.rcon_port + '\n' +
-          'rcon密码：' + config.rcon_password + '\n' +
-          '发送群名称：' + (config.mc_qq_send_group_name ? '是' : '否') + '\n' +
-          '消息同步群：' + config.group_list + '\n' +
-          '命令响应头: ' + config.command_header + '\n' +
-          '乱码修复：' + (config.is_garbled ? '是' : '否') + '\n' +
-          '屏蔽词：' + config.mask_word
-        ]
-        e.reply(str, true)
-        return true
+
+    let server_name = e.msg.replace(/#?mc(开启|关闭)同步/, '').trim()
+
+    if (!server_name) {
+      await e.reply('请输入服务器名称，发送[#mc状态]即可查看所有服务器名称')
+      return true
     }
-    if (alterFlag) {
-      try {
-        await Config.setConfig(config)
-        let msg = [
-          key.match(/(路由|端口|地址|密码|命令响应头|屏蔽词)/) ? `设置项${key}已修改为${value}`
-            : key.match(/(群名显示|rcon状态|乱码修复)/) ? `设置项${key}已${value}`
-              : key === '同步' ? `${e.group.name}(${e.group_id})与服务器消息同步已${value}`
-                : "???"
-        ]
-        Log.i('更新配置项', key, value)
-        e.reply(msg, true)
-      } catch (err) {
-        Log.e(err)
-        Log.e(err.message)
-        return this.e.reply("设置失败。请查看控制台报错", true)
+
+    const server = config.mc_qq_server_list.find(s => s.server_name === server_name);
+
+    if (!server) {
+      await e.reply(`未找到名称为${server_name}的服务器，发送[#mc状态]即可查看所有服务器名称`);
+      return true
+    }
+
+    if (e.msg.includes('开启')) {
+
+      if (!server.group_list.includes(e.group_id)) {
+        server.group_list.push(e.group_id);
       }
-      return true
-    } else {
-      e.reply(`设置项${key}无法修改为${value ? value : '空'}`, true)
+
+      if (!server.bot_self_id.includes(e.self_id)) {
+        server.bot_self_id.push(e.self_id);
+      }
+
+      if (!server.rcon_able) {
+        server.rcon_able = true;
+      }
+
+      await e.reply(`本群已开启与${server_name}的同步`);
+
+    } else if (e.msg.includes('关闭')) {
+
+      const index = server.group_list.indexOf(e.group_id);
+      if (index > -1) {
+        server.group_list.splice(index, 1);
+      }
+
+      const index2 = server.bot_self_id.indexOf(e.self_id);
+      if (index2 > -1) {
+        server.bot_self_id.splice(index2, 1);
+      }
+
+      if (!server.group_list.length) {
+        server.rcon_able = false;
+      }
+
+      Config.setConfig(config);
+      await e.reply(`本群已关闭与${server_name}的同步`);
+
     }
+    config.mc_qq_server_list[config.mc_qq_server_list.findIndex(s => s.server_name === server_name)] = server;
+    Config.setConfig(config);
+    return true
   }
 }

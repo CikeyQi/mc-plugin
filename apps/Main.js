b5510d233a042bbf7032eb888e17c4e7e639c515
ZeroCal Su <3146312184@qq.com>
2024-06-23 23:54:15 +0800

    支持正向ws连接，更改为ws方式发送消息

--- apps/Main.js
+++ apps/Main.js
@@ -1,5 +1,6 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import RconClient from '../components/Rcon.js'
+import WebSocketCilent from '../components/WebSocket.js'
 import Config from '../components/Config.js'
 
 export class Main extends plugin {
@@ -27,58 +28,71 @@ export class Main extends plugin {
 
   async sync(e) {
     if (!e.group_id) return false
-    const { mc_qq_send_group_name, mc_qq_server_list, debug_mode } = await Config.getConfig();
+    const { mc_qq_send_group_name, mc_qq_say_way, mc_qq_server_list, debug_mode } = await Config.getConfig();
     const { servers } = RconClient
-    if (!Object.keys(servers).length) return false
+    const { connections } = WebSocketCilent
 
-    const serversList = mc_qq_server_list
-      .filter(server => server.rcon_able && server.group_list.some(group => group == e.group_id));
+    if (!mc_qq_server_list.length) return false
 
-    if (!serversList.length) return false
+    const serversList = mc_qq_server_list.filter(server => server.group_list.some(group => group == e.group_id))
+    serversList.forEach(async (serverConfig, i) => {
+      if (e.raw_message.startsWith(serverConfig.command_header) && (serverConfig.command_user?.some(user => user == e.user_id) || e.isMaster)) {
+        let response = await servers[serverConfig.server_name].send(`${e.raw_message.replace(serverConfig.command_header, '')}`);
 
-    serversList
-      .map(({ server_name }) => servers[server_name])
-      .filter(server => server !== undefined)
-      .forEach(async (server, i) => {
-        if (e.raw_message.startsWith(serversList[i].command_header) && (serversList[i].command_user?.some(user => user == e.user_id) || e.isMaster)) {
-          let response = await server.send(`${e.raw_message.replace(serversList[i].command_header, '')}`);
+        if (debug_mode) {
+          logger.mark(
+            logger.blue('[Minecraft RCON Client] ') + '向 ' +
+            logger.green(serverConfig.server_name) +
+            ' 发送命令 ' + e.raw_message.replace(serverConfig.command_header, '')
+          )
+        }
 
+        if (response) {
+          const mask_word = serverConfig.mask_word;
+          response = response.replace(new RegExp(mask_word, "g"), '');
+
+          await e.reply(response);
           if (debug_mode) {
             logger.mark(
-              logger.blue('[Minecraft RCON Client] ') + '向 ' +
+              logger.blue('[Minecraft RCON Client] ') +
               logger.green(serversList[i].server_name) +
-              ' 发送命令 ' + e.raw_message.replace(serversList[i].command_header, '')
-            )
-          }
-
-          if (response) {
-            const mask_word = serversList[i].mask_word;
-            response = response.replace(new RegExp(mask_word, "g"), '');
-
-            await e.reply(response);
-            if (debug_mode) {
-              logger.mark(
-                logger.blue('[Minecraft RCON Client] ') +
-                logger.green(serversList[i].server_name) +
-                ' 返回消息 ' + response
-              )
-            }
-          }
-
-        } else {
-          let msg = mc_qq_send_group_name ? `[${e.group_name}] ` : "";
-          msg += `[${e.sender.nickname}] ${e.raw_message}`
-
-          server.send(`say ${msg}`);
-          if (debug_mode) {
-            logger.mark(
-              logger.blue('[Minecraft RCON Client] ') + '向 ' +
-              logger.green(serversList[i].server_name) +
-              ' 发送消息 ' + msg
+              ' 返回消息 ' + response
             )
           }
         }
-      })
+      } else {
+        let messages = [{
+          text: `${mc_qq_send_group_name ? `[${e.group_name}] ` : ""}[${e.sender.nickname}] ` + mc_qq_say_way,
+        }];
+
+        e.message.forEach(element => {
+          let msg = '';
+          if (element.type === 'text') {
+            msg = element.text;
+          } else if (element.type === 'image') {
+            msg = `[[CICode,url=${element.url},name=图片]]`;
+          } else {
+            msg = element.text || '';
+          }
+          messages.push({ text: msg });
+        });
+
+        connections[serverConfig.server_name].send({
+          api: "broadcast",
+          data: {
+            message_list: messages,
+          }
+        });
+
+        if (debug_mode) {
+          logger.mark(
+            logger.blue('[Minecraft RCON Client] ') + '向 ' +
+            logger.green(serverConfig.server_name) +
+            ' 发送消息 ' + messages
+          )
+        }
+      }
+    })
 
     return false
   }
